1c683e41c0eef5755800f66b653c8dadb7e8bd62
Aghmaz <aghmazofficial@gmail.com>
2025-08-30 20:34:13 +0500

    HOF code explained

--- src/Hoc.jsx
+++ src/Hoc.jsx
@@ -8,6 +8,23 @@ const UserListWithLoading = WithLoading(UserList);
 export default function Hoc() {
   const users = ["haseeb", "shahzaib", "huzaifa", "bismah"];
   const isLoading = false;
+
+  //   we are going to learn HOF - high order function
+
+  function withLogging(fn) {
+    return function (...args) {
+      console.log(args, "argument");
+      const result = fn(...args); // add => 3,4
+      console.log(result, "result");
+      return result;
+    };
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  const loggedAdd = withLogging(add);
+  loggedAdd(3, 4);
+  loggedAdd(30, 14);
   return (
     <>
       <center>
