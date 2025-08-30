88794147fb6fe7dd508642f3d1fe05b2c9096f12
Aghmaz <aghmazofficial@gmail.com>
2025-08-30 20:18:58 +0500

    comment added for better understanding

--- src/Hoc.jsx
+++ src/Hoc.jsx
@@ -3,7 +3,7 @@ import UserList from "./components/UserList";
 import WithLoading from "./components/withLoading";
 
 // usage
-
+// we are passing userList component as an props to withLoading component
 const UserListWithLoading = WithLoading(UserList);
 export default function Hoc() {
   const users = ["haseeb", "shahzaib", "huzaifa", "bismah"];
@@ -13,6 +13,7 @@ export default function Hoc() {
       <center>
         <h1>Higher order component</h1>
       </center>
+      {/* passing props to userListwithloading that is HOC */}
       <UserListWithLoading isLoading={isLoading} users={users} />
     </>
   );
--- src/components/withLoading.jsx
+++ src/components/withLoading.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default function WithLoading(WrappedComponent) {
+  // in another words we can say it as UserList component
   return function EnhancedComponent({ isLoading, ...props }) {
+    // props = users array that we were passing in the HOC
+    // we were passing these props on HOC component
     if (isLoading) {
       return (
         <div>
